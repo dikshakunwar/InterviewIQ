1ed84500f00d166c16a17d0eb0e0592429d55a54
Diksha Kunwar <dikshakunwar02@gmail.com>
2026-07-15 22:54:52 +0530

    Initial setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("HireReady API Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
